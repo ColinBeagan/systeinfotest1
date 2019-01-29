bc821b7f5c389a0ae36f2f6d5c05290ec7c43458
Colin Beagan <beagan891@gmail.com>
2019-01-29 10:11:24 +0000

    dsg

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="systeminfotest", 
+setup(name="systeminfotest1", 
       version="0.1", 
       description="Basic system information for COMP30830", 
       url="",
       authors="Colin Beagan",
       author_email="colin.beagan@ucdconnect.ie", 
       license="GPL3", 
-      packages=['systeminfotest'],
+      packages=['systeminfotest1'],
       entry_points={
-          'console_scripts':['comp30830_systeminfo=systeminfotest.main:main']
+          'console_scripts':['comp30830_systeminfo=systeminfotest1.main:main']
           }
       )

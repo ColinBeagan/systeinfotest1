4798660b0e0e9fe8929d9dcfd02f12a753146ef0
Colin Beagan <beagan891@gmail.com>
2019-01-29 09:57:58 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="systeminfotest", 
+      version="0.1", 
+      description="Basic system information for COMP30830", 
+      url="",
+      authors="Colin Beagan",
+      author_email="colin.beagan@ucdconnect.ie",  
+      license="GPL3", 
+      packages=['systeminfotest'],
+      entry_points={
+          'console_scripts':['comp30830_systeminfo=systeminfotest.main:main']
+          }
+      )
---
+++ systeminfotest/main.py
@@ -0,0 +1,14 @@
+'''
+Created on 22 Jan 2019
+
+@author: beaga
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return 
+# comment
+
+if __name__ == '__main__':
+    main()
